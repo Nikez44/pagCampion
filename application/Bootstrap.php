915a82f3006397cb5b8bf39ad2edee0a94c754e9
Nikez44 <nike-o_94@hotmail.com>
2015-02-17 11:14:37 -0600

    admin module added, but dont show his layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,7 +1,36 @@
 <?php
+require_once '../library/Plugins/Layout.php';
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+
+   /* protected function _initLoaderResource()
+    {
+        $resourceLoader = new Zend_Loader_Autoloader_Resource(array(
+            'basePath'  => APPLICATION_PATH . '/../application',
+            'namespace' => 'Application'
+        ));
+
+        $modules = array("admin");
+
+        foreach ($modules as $module){
+            $autoLoader = new Zend_Application_Module_Autoloader(array(
+                "namespace"=>ucfirst($module),
+                "basePath"=>APPLICATION_PATH."/modules/".strtolower($module)
+            ));
+        }
+    }
+
+    protected function _initPlugins(){
+        $this->bootstrap('frontController');
+        //$this->bootstrap('loadAclConfig');
+
+        $plugin = new Controller_Plugin_Layout();
+        $this->frontController->registerPlugin($plugin);
+
+        $plugin = new Controller_Plugin_Acl();
+        $this->frontController->registerPlugin($plugin);
+    }*/
 
 }
 
---
+++ application/modules/admin/controllers/indexController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 17/02/2015
+ * Time: 10:42 AM
+ */
+
+class Admin_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $this->view->controller = $this->getRequest()->getControllerName();
+        $this->view->action = $this->getRequest()->getActionName();
+    }
+
+    public function indexAction(){
+
+    }
+
+}
