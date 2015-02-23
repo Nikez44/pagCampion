939925869a44b58edb867ce5738346b8e26067cc
Nikez44 <nike-o_94@hotmail.com>
2015-02-23 12:10:50 -0600

    login in admin module its working

--- application/modules/admin/controllers/indexController.php
+++ application/modules/admin/controllers/indexController.php
@@ -9,6 +9,17 @@
 class Admin_IndexController extends Zend_Controller_Action
 {
 
+    public function preDispatch()
+    {
+        if (!Application_Service_Authentication::getInstance()->isAuthentication()) {
+                $this->_redirect('/admin/login');
+        }else{
+            if(Application_Service_Authentication::getInstance()->getTypeUser()==2){
+                $this->_redirect('/admin/login');
+            }
+        }
+    }
+
     public function init()
     {
         $this->view->controller = $this->getRequest()->getControllerName();
